6ba4ce66479ecfa7df5be3f3d93a7b9ff2713639
KanishqGandharv219 <kanishqgandharv@gmail.com>
2026-02-27 16:40:19 +0530

    Cleanup: Remove temporary setup script

--- repo_setup.bat
+++
@@ -1,5 +0,0 @@
-git init
-git remote add origin "https://github.com/KanishqGandharv219/FactorySegMaster.git"
-git add .
-git commit -m "Finalize Day 1: Root structure and OpenCV code"
-git push -f origin main
